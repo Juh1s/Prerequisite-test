61f5ed09bac719023201f6eeb21bbc34934c1e8c
Firstname Lastname <username@aalto.fi>
2019-04-23 13:53:57 +0300

    Improved commenting.

--- Prerequisite-test.git/abundant.cc
+++ Prerequisite-test.git/abundant.cc
@@ -5,6 +5,13 @@
 #include <cstdlib>
 using namespace std;
 
+ /*Function that prints abundant numbers
+ e.a. numbers with their proper divisors
+ being larger than the number itself.
+ example: number 12
+    Proper divisors for 12 are 1, 2, 3, 4 and 6.
+    The sum of these is 1 + 2 + 3 + 4 + 6 = 16.
+    16 > 12; therefore 12 is an abundant number.*/
 void abundant(int n)
 {
   int aCount = 0;
@@ -14,23 +21,21 @@ void abundant(int n)
     dSum = 0;
      //Counts the divisor sum.
     for(int u = 1; u < i; u++){
-      //printf(" divider: %d  | ", u);
       if(i%u == 0) {
         dSum = dSum + u;
       };
     };
-     //Prints abundant numbers.
+     //Prints abundant numbers but not other numbers.
     if(dSum > i){
       cout << i << "\n";
-      //printf("\n %d is Abundant Number   ||  aCount is %d\n\n", i, aCount);
       aCount++;
     };
-    //printf("Number = %d | Sum = %d | %d < %d\n\n", i, dSum, aCount, n);
     i++;
   };
   return;
 }
 
+ //Main loop.
 int main(int argc, char* argv[]) {
   if(argc > 1){
     int number = atoi(argv[1]);

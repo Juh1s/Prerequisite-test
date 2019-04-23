3c1077895e3f883db7792e32a5c5d0acaad41523
Firstname Lastname <username@aalto.fi>
2019-04-23 10:03:09 +0300

    First completed iteration.

--- Prerequisite-test.git/abundant.cc
+++ Prerequisite-test.git/abundant.cc
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
+#include <set>
+using namespace std;
 
 void AbundantNum(int n)
 {
-  int a = 0;
-  while (a < n) {
-    if(a){
+  int aCount = 0;
+  while (aCount < n) {
+    int i = 2;
+    int dSum = 0;
 
+     //Counts the divisor sum.
+    for(int u = 1; u < i; u++){
+      if(i%u == 0) {
+        dSum = dSum + u;
+      };
     };
+     //Prints abundant numbers.
+    if(dSum > i){
+      cout << i << "\n";
+      aCount++;
+    };
+    i++;
   };
-  printf("\n");
 }
